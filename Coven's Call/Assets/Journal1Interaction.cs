5bf73c8e161fb0b700a2ff2b84118d63836c0cc6
elenagem <elena.g192001@yahoo.com>
2025-05-24 15:51:18 +0300

    journal page 1 interaction

---
+++ Coven's Call/Assets/Journal1Interaction.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Journal1Interaction : MonoBehaviour
+{
+    public GameObject interactionPrompt;
+    public KeyCode interactionKey = KeyCode.X;
+    public KeyCode exitKey = KeyCode.Escape;
+    private bool isPlayerNear = false;
+
+    void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(interactionKey))
+        {
+            interactionPrompt.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(exitKey))
+        {
+            interactionPrompt.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interactionPrompt.SetActive(true);
+            isPlayerNear = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interactionPrompt.SetActive(false);
+            isPlayerNear = false;
+        }
+    }
+}
