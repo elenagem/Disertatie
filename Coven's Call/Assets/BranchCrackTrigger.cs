eee1a9ed37cfe5edef514881b8895084946552a7
elenagem <elena.g192001@yahoo.com>
2025-05-28 02:14:30 +0300

    witch run

--- Coven's Call/Assets/BranchCrackTrigger.cs
+++ Coven's Call/Assets/BranchCrackTrigger.cs
@@ -4,12 +4,11 @@ using System.Collections;
 public class BranchCrackTrigger : MonoBehaviour
 {
     public AudioSource crackAudio;
+    public AudioSource chantAudio;
     public GameObject player;
     public GameObject thirdPersonCamera;
     public Transform cameraTransform;
     public float moveDuration = 1.5f;
-
-    // (Optional) daca vrei sa apara o creanga vizuala
     public GameObject branchPrefab;
 
     private ManualThirdPersonCamera cameraController;
@@ -34,8 +33,7 @@ public class BranchCrackTrigger : MonoBehaviour
         if (triggered) return;
         triggered = true;
 
-        // Dupa 1 sec de la replica
-        Invoke(nameof(PlayCrackAndZoom), 0.1f);
+        Invoke("PlayCrackAndZoom", 0.1f);
     }
 
     void PlayCrackAndZoom()
@@ -50,21 +48,20 @@ public class BranchCrackTrigger : MonoBehaviour
         if (controller != null)
             controller.enabled = false;
 
-        // Calculeaza pozitia jos la picioarele jucatorului
         Vector3 playerFeet = player.transform.position + player.transform.forward * 0.4f + Vector3.down * 0.9f;
         Quaternion camRotation = Quaternion.Euler(90f, player.transform.eulerAngles.y, 0f);
 
-        // Creeaza target pentru camera
         GameObject cameraPoint = new GameObject("DynamicZoomTarget");
-        cameraPoint.transform.position = playerFeet + new Vector3(0f, 0.5f, 0f); // putin deasupra solului
+        cameraPoint.transform.position = playerFeet + new Vector3(0f, 0.5f, 0f);
         cameraPoint.transform.rotation = camRotation;
 
-        // Instantiaza crenguta (optional)
         if (branchPrefab != null)
             Instantiate(branchPrefab, playerFeet, Quaternion.identity);
 
-        // Zoom
         StartCoroutine(MoveCamera(cameraPoint.transform.position, cameraPoint.transform.rotation));
+
+        // Dupa ce a ajuns camera pe creanga, asteapta 2 secunde si revine
+        Invoke("ExitBranchZoom", 2.5f);
     }
 
     IEnumerator MoveCamera(Vector3 targetPos, Quaternion targetRot)
@@ -84,6 +81,46 @@ public class BranchCrackTrigger : MonoBehaviour
 
         cameraTransform.position = targetPos;
         cameraTransform.rotation = targetRot;
+    }
 
+    void ExitBranchZoom()
+    {
+        if (chantAudio != null)
+            chantAudio.Stop();
+
+        StartCoroutine(ResetCameraAndControl());
+    }
+
+    IEnumerator ResetCameraAndControl()
+    {
+        float elapsedTime = 0f;
+        Vector3 startPos = cameraTransform.position;
+        Quaternion startRot = cameraTransform.rotation;
+
+        while (elapsedTime < moveDuration)
+        {
+            float t = elapsedTime / moveDuration;
+            cameraTransform.position = Vector3.Lerp(startPos, originalPosition, t);
+            cameraTransform.rotation = Quaternion.Slerp(startRot, originalRotation, t);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        cameraTransform.position = originalPosition;
+        cameraTransform.rotation = originalRotation;
+
+        if (cameraController != null)
+            cameraController.enabled = true;
+
+        var controller = player.GetComponent<SimplePlayerController>();
+        if (controller != null)
+            controller.enabled = true;
+
+        // Activeaza comportamentul vrajitoarelor
+        var witchBehavior = Object.FindFirstObjectByType<WitchBehavior>();
+        if (witchBehavior != null)
+        {
+            witchBehavior.TriggerWitchTurn();
+        }
     }
 }
---
+++ Coven's Call/Assets/WitchBehavior.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WitchBehavior : MonoBehaviour
+{
+    public List<GameObject> witches;
+    public GameObject player;
+    public AudioSource screamAudio;
+
+    public void TriggerWitchTurn()
+    {
+        foreach (var witch in witches)
+        {
+            Vector3 lookTarget = new Vector3(player.transform.position.x, witch.transform.position.y, player.transform.position.z);
+            witch.transform.LookAt(lookTarget);
+        }
+
+        StartCoroutine(DelayedChase());
+    }
+
+    IEnumerator DelayedChase()
+    {
+        yield return new WaitForSeconds(1f);
+
+        if (screamAudio != null)
+            screamAudio.Play();
+
+        foreach (var witch in witches)
+        {
+            NavMeshAgent agent = witch.GetComponent<NavMeshAgent>();
+            if (agent != null)
+            {
+                agent.isStopped = false;
+                agent.SetDestination(player.transform.position);
+            }
+
+            Animator anim = witch.GetComponent<Animator>();
+            if (anim != null)
+            {
+                anim.SetBool("isRunning", true);
+            }
+        }
+    }
+}
